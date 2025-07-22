8281d1bbff9a10352a14edeaff9a40c44bb3448c
arta-ademaj <85793710+arta-ademaj@users.noreply.github.com>
2025-07-22 16:13:22 +0200

    Models and migrations files created.
    Models and migrations files created.

---
+++ database/migrations/2025_07_22_135450_create_loans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->foreign('member_id')->references('id')->on('members');
+            $table->dateTime('loaned_at')->nullable();
+            $table->dateTime('due_at')->nullable();
+            $table->dateTime('returned_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+}
